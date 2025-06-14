a9795968352a2a3286e763c5fba1969c8020f9a7
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-14 10:24:31 +0530

    created some custom method for our application

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/repository/ChildrenRepository.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/repository/ChildrenRepository.java
@@ -1,5 +1,7 @@
 package com.mvcwebapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.mvcwebapp.entity.Children;
 
 @Repository
 public interface ChildrenRepository extends JpaRepository<Children, Integer>{
-
+	
+	public List<Children> findByName(String name);
+	
+	public List<Children> findByAge(int age);
 }
