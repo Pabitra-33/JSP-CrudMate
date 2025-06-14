0e1f8a7ff96ac98a18201122e35cf6e6e01aa41f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-14 10:23:46 +0530

    providing implementation for the abstract methods created in the service interface

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/service/ChildrenServiceImplementation.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/service/ChildrenServiceImplementation.java
@@ -1,5 +1,34 @@
 package com.mvcwebapp.service;
 
-public class ChildrenServiceImplementation {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mvcwebapp.entity.Children;
+import com.mvcwebapp.repository.ChildrenRepository;
+
+@Service
+public class ChildrenServiceImplementation implements ChildrenService {
+
+	@Autowired
+	private ChildrenRepository chRepository;
+	
+	
+	@Override
+	public void saveChildren(Children child) {
+		chRepository.save(child);
+	}
+
+	
+	@Override
+	public List<Children> getByUsingName(String name) {
+		return chRepository.findByName(name);
+	}
+
+	
+	@Override
+	public List<Children> getByUsingAge(int age) {
+		return chRepository.findByAge(age);
+	}
 }
