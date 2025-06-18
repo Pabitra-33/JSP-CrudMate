ecd59eaeb2d881a1f701e3d0e5a5ba5b8672d1cb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-18 12:01:26 +0530

    creating repository interface to interact with database

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/repository/StudentRepository.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/repository/StudentRepository.java
@@ -1,5 +1,11 @@
 package com.mvcwebapp.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mvcwebapp.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
 
 }
