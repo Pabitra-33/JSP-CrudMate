2164bd9ea5a84332201efb117a794216c76762ac
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-18 12:00:40 +0530

    providing implementation for the service interface abstract methods

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentServiceImplementation.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentServiceImplementation.java
@@ -1,5 +1,19 @@
 package com.mvcwebapp.service;
 
-public class StudentServiceImplementation {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.mvcwebapp.entity.Student;
+import com.mvcwebapp.repository.StudentRepository;
+
+@Service
+public class StudentServiceImplementation implements StudentService{
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void saveStudent(Student student) {
+		studentRepository.save(student);
+	}
 }
