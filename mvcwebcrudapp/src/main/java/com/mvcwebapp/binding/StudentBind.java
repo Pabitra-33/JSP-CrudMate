b750e2f9827178eb26e4390b0994b6dd8a6a3065
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-20 22:43:46 +0530

    making some changes in the binding class

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/binding/StudentBind.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/binding/StudentBind.java
@@ -3,6 +3,7 @@ package com.mvcwebapp.binding;
 import java.util.Arrays;
 
 //creating this class to bind the data from the fronted data in this class attributes for later use.
+
 public class StudentBind {
 
 	private Integer sid;
