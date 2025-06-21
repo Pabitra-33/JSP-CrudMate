847d9479d91a24b46ec8f520529ccf56be64a05d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-21 06:21:28 +0530

    updated the abstract method in the interface

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentService.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentService.java
@@ -3,6 +3,6 @@ package com.mvcwebapp.service;
 import com.mvcwebapp.entity.Student;
 
 public interface StudentService {
-	
+	//creating a abstract method to save student data
 	public void saveStudent(Student student);
 }
