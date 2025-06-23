d997c472bee305fe77e69f9f9143e5c2356c9042
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-23 22:34:59 +0530

    created an abstract method for the deletion of Students

--- mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentService.java
+++ mvcwebcrudapp/src/main/java/com/mvcwebapp/service/StudentService.java
@@ -5,4 +5,6 @@ import com.mvcwebapp.entity.Student;
 public interface StudentService {
 	//creating a abstract method to save student data
 	public void saveStudent(Student student);
+	
+	public void deleteStudent(int id);
 }
